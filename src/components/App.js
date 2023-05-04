e51977f62d9335b3c328576ca98a80b57706aa4a
AitorFagoaga <aitor.fagoaga2001@gmail.com>
2023-05-04 12:24:59 -0300

    topBar

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import '../App.css';
+import * as React from 'react';
+import TopBar from "./TopBar"
+
+function App() {
+  return (
+    <div className="App">
+      
+      <TopBar/>
+    </div>
+  );
+}
+
+
+export default App;
